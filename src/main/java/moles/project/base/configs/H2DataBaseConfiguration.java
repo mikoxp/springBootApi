48ce2fd87d78c66c981cd2c13a19f013755fd439
olesmichalxp@gmail.com <olesmichalxp@gmail.com>
2017-11-29 17:32:23 +0100

    Init

---
+++ src/main/java/moles/project/base/configs/H2DataBaseConfiguration.java
@@ -0,0 +1,17 @@
+package moles.project.base.configs;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class H2DataBaseConfiguration {
+
+    @Bean
+    ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/h2/client/*");
+        return registrationBean;
+    }
+}
