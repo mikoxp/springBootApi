5fc5f8a24463e5b39eb7360cacf81b0f4c1d7e89
olesmichalxp@gmail.com <olesmichalxp@gmail.com>
2017-11-29 19:00:32 +0100

    pom and readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# springBootBase
+# http://localhost:8066/v2/api-docs
+# http://localhost:8066/swagger-ui.html
+
--- src/main/java/moles/project/base/configs/H2DataBaseConfiguration.java
+++ src/main/java/moles/project/base/configs/H2DataBaseConfiguration.java
@@ -5,6 +5,9 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author michal.oles
+ */
 @Configuration
 public class H2DataBaseConfiguration {
 
