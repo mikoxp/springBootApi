6503808d555e82b7c1d8fffe2079f873302c6e44
olesmichalxp@gmail.com <olesmichalxp@gmail.com>
2017-12-02 12:05:03 +0100

    add @author

--- src/main/java/moles/project/base/configs/SwaggerConfiguration.java
+++ src/main/java/moles/project/base/configs/SwaggerConfiguration.java
@@ -9,7 +9,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- * @author michal.oles 
+ * @author michal.oles
  */
 @Configuration
 @EnableSwagger2
