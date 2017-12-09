19c2f92bb8ec6ccc2494d40e9c9318dd9d16adfd
olesmichalxp@gmail.com <olesmichalxp@gmail.com>
2017-12-09 17:19:35 +0100

    Add logger

--- src/main/java/moles/project/base/controllers/PersonController.java
+++ src/main/java/moles/project/base/controllers/PersonController.java
@@ -2,6 +2,8 @@ package moles.project.base.controllers;
 
 import moles.project.base.entities.Person;
 import moles.project.base.services.PersonService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,29 +12,44 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("person")
 public class PersonController {
 
     @Autowired
     private PersonService personService;
+    private static final Logger logger = LoggerFactory.getLogger(PersonController.class);
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String test() {
-        return "Test";
-    }
-
+    /**
+     *
+     * @return all people
+     */
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     public List<Person> getAll(){
+        logger.info("Get all people");
         return personService.getAll();
     }
 
+    /**
+     *
+     * @param name name
+     * @return search people
+     */
     @RequestMapping(value = "/search/name/{name}", method = RequestMethod.GET)
     public List<Person> searchByName(@PathVariable String name){
+        logger.info("Search people: "+name);
         return personService.searchByName(name);
     }
+
+    /**
+     *
+     * @param name name
+     * @return search people
+     */
     @RequestMapping(value = "/search/spec/name/{name}", method = RequestMethod.GET)
-    public List<Person> searchByNameSpecyfication(@PathVariable String name){
+    public List<Person> searchByNameSpec(@PathVariable String name){
+        logger.info("Search people: "+name);
         return personService.searchByNameSpec(name);
     }
 
